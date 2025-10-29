52a10be9355fe371b8b5d6c339a60c7433d1155c
hadaz27 <aziz.haddad003@gmail.com>
2025-10-29 14:58:33 +0100

    add : improved items placements and adding svg icons

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
-let div = document.getElementById("solde")
-let balance = querySelector("p")
+const popup_add = document.getElementById("add_popup")
+const open_btn = document.getElementById("add_button")
+const close_btn = document.getElementById("close_button")
+const add_income_btn = document.getElementById("add_income_button")
+
+
+open_btn.addEventListener("click", () => {
+    popup_add.classList.toggle("hidden")
+})
+
+close_btn.addEventListener("click", () => {
+    popup_add.classList.toggle("hidden")
+})
+
+
